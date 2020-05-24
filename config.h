13fa71a6aed4015df6b1b466b7d0d1aeb29b6e78
Alex <alex@LOL.LOL>
2020-05-23 23:17:18 -0500

    Change colorscheme

--- config.h
+++ config.h
@@ -6,15 +6,27 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char black[]           = "#282C34";
+static const char lighter_black[]   = "#646E82";
+static const char gray[]            = "#ABB2BF";
+static const char blue[]            = "#61AFEF";
+static const char red[]             = "#E06C75";
+static const char purple[]          = "#C678DD";
+static const char green[]           = "#98C379";
+static const char yellow[]          = "#E5C07B";
+static const char cyan[]            = "#56B6C2";
+static const char white[]           = "#F0F2F4";
+
+static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { gray,  black },
+	[SchemeSel]  = { white, red   },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
